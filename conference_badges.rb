f5def41575da8810bbe6989cd5cd59180bdffd45
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 17:10:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,14 +5,14 @@ end
 
 def batch_badge_creator(arr1)
   arr2 = []
-  arr1.each {|i| puts arr2 << "Hello, my name is #{i}."}
+  arr1.each {|i| arr2 << "Hello, my name is #{i}."}
   arr2
 end
 
 def assign_rooms(speakers)
   rooms = []
   speakers.each_with_index do |speaker, index| 
-    puts rooms << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    rooms << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
   end
   rooms
 end
